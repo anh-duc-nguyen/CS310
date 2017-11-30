8a15856b5d4bd2dffa89b7c0ddb9c7ff510917d7
anhdnguyenn <nguyenducanh@aircuanguyen2.truman.edu>
2017-11-30 08:06:13 -0600

    meh

--- Sorting/merge_sort.py
+++ Sorting/merge_sort.py
@@ -1,4 +1,42 @@
 import random
+def merge(A,l,m,r):
+    n1 = m - l +1
+    n2 = r - m
+    L = A[l:n1]
+    R = A[n2:r]
+    
+    i = 0
+    j = 0
+    k = 1
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i+=1
+        else:
+            A[k] = R[j]
+            j+=1
+        k +=1
+    while i<n1:
+        A[k] = L[i]
+        i += 1 
+        k += 1
+    while j<n2:
+        A[k] = R[j]
+        j+=1
+        k+=1
+def Merge(A,l,m,r):
+    L = A[l:m]
+    R = A[m:]
+
+
+def merge_sort(A,l,r):
+    if l<r:
+        m = (l+r-1)//2
+        merge_sort(A,l,m)
+        merge_sort(A,m+1,r)
+        merge(A,l,m,r)
+
+
 
 
 def merge_Sort(alist):
@@ -22,7 +60,7 @@ def test():
 
 	print ('unsorted list is: ',alist)
 	print ('sorting')
-	alist = merge_Sort(alist)
+	merge_sort(alist,0,len(alist)-1)
 	print ('list after sorting: ', alist)
 
 test()
